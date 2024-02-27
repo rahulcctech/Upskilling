65dda2f33d868edf36bf53e4922e385b6987428e
rahul.roy <rahul.roy@cctech.co.in>
2024-02-27 16:49:23 +0530

    Add project files.

---
+++ ReflactionDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the assembly containing the Employee class
+            Assembly assembly = Assembly.LoadFrom(@"E:\Project\TestReflaction\TestReflaction\bin\Debug\net6.0\TestReflaction.dll");
+
+            // Get the type of the Employee class
+            Type employeeType = assembly.GetType("EmployeeLibrary.Employee");
+
+            // Create an instance of the Employee class
+            object employeeInstance = Activator.CreateInstance(employeeType);
+
+            // Set values for properties using reflection
+            PropertyInfo nameProperty = employeeType.GetProperty("Name");
+            nameProperty.SetValue(employeeInstance, "Rahul Roy");
+
+            PropertyInfo ageProperty = employeeType.GetProperty("Age");
+            ageProperty.SetValue(employeeInstance, 24);
+
+            PropertyInfo genderProperty = employeeType.GetProperty("Gender");
+            genderProperty.SetValue(employeeInstance, "Male");
+
+            // Call the PrintDetails method using reflection
+            MethodInfo printDetailsMethod = employeeType.GetMethod("PrintDetails");
+            printDetailsMethod.Invoke(employeeInstance, null);
+        }
+    }
+}
---
+++ TestReflaction/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+        }
+    }
+}
